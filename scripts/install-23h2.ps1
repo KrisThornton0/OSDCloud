ca9becf10f01fe1dd990594ed0ff2ca36d5f2c5c
KrisThornton0 <kris.thornton@sonova.com>
2025-03-18 07:41:57 -0700

    Create install-23h2.ps1

---
+++ scripts/install-23h2.ps1
@@ -0,0 +1,29 @@
+# Settings
+$OSName = 'Windows 11 23H2 x64'
+$OSEdition = 'Enterprise'
+$OSActivation = 'Volume'
+$OSLanguage = 'en-us'
+
+$TimeServerUrl = "pool.ntp.org"
+
+#Set Global OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$True
+    RecoveryPartition = [bool]$True
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$True
+    WindowsUpdateDrivers = [bool]$True
+    WindowsDefenderUpdate = [bool]$True
+    SetTimeZone = [bool]$True
+    ClearDiskConfirm = [bool]$True
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$True
+    CheckSHA1 = [bool]$True
+}
+
+# Set the time
+$DateTime = (Invoke-WebRequest -Uri $TimeServerUrl -UseBasicParsing).Headers.Date
+Set-Date -Date $DateTime
+
+Write-Host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
